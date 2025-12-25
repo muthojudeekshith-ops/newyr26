ad973581aa7f4c86d96d5397b83ea4efbb5df357
Deekshith Muthoju <muthojudeekshith@gmail.com>
2025-12-26 02:41:41 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,19 @@
+const i1 = document.getElementById("i1");
+const i2 = document.getElementById("i2");
+const i3 = document.getElementById("i3");
+
+function goToI2(){
+  i1.classList.remove("show");
+  i2.classList.add("show");
+}
+
+function checkPassword(){
+  const pwd = document.getElementById("pwd").value;
+  if(pwd === "c5d9"){
+    i2.classList.remove("show");
+    i3.classList.add("show");
+  }else{
+    alert("Wrong Password");
+  }
+}
 
